ea569f516cf466272c4b02a2dd095ce54f21f707
vinaysaikhandapu <130208894+vinaysaikhandapu@users.noreply.github.com>
2023-04-27 21:27:19 +0530

    Add files via upload

---
+++ task04.js
@@ -0,0 +1,7 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
